b8c04e3d10af2e931545b569b565d61e79d06dae
Alex J. Champandard <445208+alexjc@users.noreply.github.com>
2020-05-07 07:39:25 +0200

    🏁 Test for checkpoints on multiple / all layers.

--- tests/test_checkpoint.py
+++ tests/test_checkpoint.py
@@ -10,20 +10,26 @@ from encoders.models import ALL_MODELS, ALL_LAYERS
 
 @pytest.fixture
 def images(device):
-    return torch.empty((1, 3, 256, 256), dtype=torch.float16, device=device).normal_()
+    result = torch.empty((1, 3, 32, 32), dtype=torch.float32, device=device).normal_()
+    return result.requires_grad_(True)
 
 
 @pytest.mark.cuda
 @pytest.mark.parametrize("model_class", ALL_MODELS)
 class TestMemoryUsage:
     @pytest.mark.parametrize("layer", ALL_LAYERS)
-    def test_checkpoint(self, model_class, device, images, layer):
-        model = model_class(pretrained=False).to(device=device, dtype=torch.float16)
-        images.requires_grad_(True)
-
+    def test_one_layer(self, model, images, layer):
         allocated = torch.cuda.memory_allocated()
         results = dict(model.extract(images, layers=[layer], as_checkpoints=True))
         delta = torch.cuda.memory_allocated() - allocated
 
         size = sum(r.storage().size() for r in results.values())
-        assert (size * 2) / delta == 1.0
+        assert (size * 4) / delta == 1.0
+
+    def test_all_layers(self, model, images):
+        allocated = torch.cuda.memory_allocated()
+        results = dict(model.extract(images, layers=ALL_LAYERS, as_checkpoints=True))
+        delta = torch.cuda.memory_allocated() - allocated
+
+        size = sum(r.storage().size() for r in results.values())
+        assert (size * 4) / delta == 1.0
