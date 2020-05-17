f9bd78ee433c89d1166db8f77b553e5a98594d43
Alex J. Champandard <445208+alexjc@users.noreply.github.com>
2020-05-17 20:33:37 +0200

    🧠 Pretrained models now have normalized weights & biases.

--- src/encoders/models.py
+++ src/encoders/models.py
@@ -19,7 +19,7 @@ def ConvBlock(in_channels, out_channels, activation="ReLU"):
 class VGG19(Encoder):
     CONFIG = [M(f=64, b=2), M(f=128, b=2), M(f=256, b=4), M(f=512, b=4), M(f=512, b=4)]
     FILENAME = "v0.1/vgg19"
-    HEXDIGEST = "fd09b2e521af4375dc5e2a47cbe4da75"
+    HEXDIGEST = "29e01ad88d45dcc69672b816e400760a"
 
     def __init__(self, block_type=ConvBlock, pool_type=nn.MaxPool2d, **kwargs):
         super(VGG19, self).__init__(block_type, pool_type, input_type="RGB", **kwargs)
@@ -28,7 +28,7 @@ class VGG19(Encoder):
 class VGG16(Encoder):
     CONFIG = [M(f=64, b=2), M(f=128, b=2), M(f=256, b=3), M(f=512, b=3), M(f=512, b=3)]
     FILENAME = "v0.1/vgg16"
-    HEXDIGEST = "1be9714ee6c3508820ee6d0758aaeb01"
+    HEXDIGEST = "a11733085f299b8c7933e9140d7ea3c4"
 
     def __init__(self, block_type=ConvBlock, pool_type=nn.MaxPool2d, **kwargs):
         super(VGG16, self).__init__(block_type, pool_type, input_type="RGB", **kwargs)
@@ -37,7 +37,7 @@ class VGG16(Encoder):
 class VGG13(Encoder):
     CONFIG = [M(f=64, b=2), M(f=128, b=2), M(f=256, b=2), M(f=512, b=2), M(f=512, b=2)]
     FILENAME = "v0.1/vgg13"
-    HEXDIGEST = "4aef6aa089421b12097c947f2f8257b4"
+    HEXDIGEST = "22e6a1292435b04ba3273300c9e867aa"
 
     def __init__(self, block_type=ConvBlock, pool_type=nn.MaxPool2d, **kwargs):
         super(VGG13, self).__init__(block_type, pool_type, input_type="RGB", **kwargs)
@@ -46,7 +46,7 @@ class VGG13(Encoder):
 class VGG11(Encoder):
     CONFIG = [M(f=64, b=1), M(f=128, b=1), M(f=256, b=2), M(f=512, b=2), M(f=512, b=2)]
     FILENAME = "v0.1/vgg11"
-    HEXDIGEST = "28632f54ab04986aa0f530034f5a4f96"
+    HEXDIGEST = "2898532ef3f0910dfb1634482a1ca4ef"
 
     def __init__(self, block_type=ConvBlock, pool_type=nn.MaxPool2d, **kwargs):
         super(VGG11, self).__init__(block_type, pool_type, input_type="RGB", **kwargs)
@@ -55,7 +55,7 @@ class VGG11(Encoder):
 class ThinetSmall(Encoder):
     CONFIG = [M(f=32, b=2), M(f=64, b=2), M(f=128, b=3), M(f=256, b=3), M(f=512, b=3)]
     FILENAME = "v0.1/thinet_small"
-    HEXDIGEST = "bbd2c25d2e8f77ffee09d1e2d460574f"
+    HEXDIGEST = "f15444e14276f8f36d162ee9d97ec54b"
 
     def __init__(self, block_type=ConvBlock, pool_type=nn.MaxPool2d, **kwargs):
         super(ThinetSmall, self).__init__(
@@ -66,7 +66,7 @@ class ThinetSmall(Encoder):
 class ThinetTiny(Encoder):
     CONFIG = [M(f=16, b=2), M(f=32, b=2), M(f=64, b=3), M(f=128, b=3), M(f=128, b=2)]
     FILENAME = "v0.1/thinet_tiny"
-    HEXDIGEST = "0bf93a6387f45ac1e15f3483d8d71dcc"
+    HEXDIGEST = "1e33d824b47a850485a2e85c29d5ddf0"
 
     def __init__(self, block_type=ConvBlock, pool_type=nn.MaxPool2d, **kwargs):
         super(ThinetTiny, self).__init__(
